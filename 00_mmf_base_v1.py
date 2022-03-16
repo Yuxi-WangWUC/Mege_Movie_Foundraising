169fd4dc11e834b6f943e54a4efdfb899144776c
Yuxi-WangWUC <101371589+Yuxi-WangWUC@users.noreply.github.com>
2022-03-16 14:15:37 +1300

    Add files via upload

---
+++ 00_mmf_base_v1.py
@@ -0,0 +1,27 @@
+#import statements
+
+#functions go here
+
+#""""""""""Main Routine""""""""""
+
+#Set up directions / lists needed to hold data
+
+#Ask user if they have used the program before & show instruction if necessary
+
+#Loop to get ticket details
+
+    #Get name (can't be blank)
+
+    #Get age (between 12 and 130)
+
+    #Calculte ticket price
+
+    #Loop to ask for snacks
+
+    #Calculate snack price
+
+    #Ask for payment method (and apply surcharge if necessary)
+
+#Calculater total sales and profit
+
+#Output data to text file
