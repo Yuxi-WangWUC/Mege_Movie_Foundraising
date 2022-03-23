79e5fa8818f6392c06fbbce97eda0a5a898e793c
Yuxi-WangWUC <101371589+Yuxi-WangWUC@users.noreply.github.com>
2022-03-24 11:36:40 +1300

    Add files via upload

--- 02_ticket_loop_v2.py
+++ 02_ticket_loop_v2.py
@@ -6,9 +6,14 @@ count = 0
 MAX_TICKETS = 5
 
 while name != "X" and count < MAX_TICKETS:
-    print("You have {} seats "
-          "left".format(MAX_TICKETS-count))
-
+    #tell user how many seats are left
+    if count < MAX_TICKETS - 1:
+        print("You have {} seats "
+              "left".format(MAX_TICKETS - count))
+    # Warns users that only one seat is left
+    else:
+         print("You have one seat left")
+         
     # Get details...
     name = input("Name : ")
     count += 1
@@ -16,6 +21,10 @@ while name != "X" and count < MAX_TICKETS:
     
 if count == MAX_TICKETS:
     print("You had sold all the available tickets")
+elif count == MAX_TICKETS - 1:
+    print("You had sold {} tickets.  \n"
+          "There one {} place still available"
+          .format(count,MAX_TICKETS - count))
 else:
     print("You have sold {} tickets.  \n"
           "There are {} places still available"
